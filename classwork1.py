55aaa49640f59ec6f1c7dd779bdf56d9662ddc7a
StorozhenkoMykola <storozhenko.mykola.92@gmail.com>
2018-12-02 23:22:11 +0200

    classwork1

--- classwork1.py
+++ classwork1.py
@@ -3,3 +3,16 @@ b=int(input("enter number b = "))
 if (b>a): print("bigger number b = "+str(b))
 elif (b<a): print("bigger number a = "+str(a))
 else:print ("a=b")
+
+
+a=int(input("enter number "))
+if (a%2): print("Не парне число")
+else:print("Парне число")
+
+a=int(input("enter number = "))
+i = 1
+f = 1
+while i <= a:
+    f *= i
+    i += 1
+print("factorial of number ",a, " = ", f)
